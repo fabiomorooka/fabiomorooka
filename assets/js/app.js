cd2dcabc4b3c0fc7c9205db467954f7e8708a2b9
fabiomorooka <fabio.morooka@gmail.com>
2020-05-10 23:20:56 +0200

    insert skill and award page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
 $(document).foundation()
+
+/*
+// more click
+$('.card-profile-stats-more-link').click(function(e){
+  e.preventDefault();
+  if ( $(".card-profile-stats-more-content").is(':hidden') ) {
+    $('.card-profile-stats-more-link').find('i').removeClass('fi-arrow-down').addClass('fi-arrow-up');
+  } else {
+    $('.card-profile-stats-more-link').find('i').removeClass('fi-arrow-up').addClass('fi-arrow-down');
+  }
+  $(this).next('.card-profile-stats-more-content').slideToggle();
+});
+
+*/
