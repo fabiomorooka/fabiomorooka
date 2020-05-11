b7fccd88507cf7f3a33ec655c62e3404fc2b44fe
fabiomorooka <fabio.morooka@gmail.com>
2020-05-12 01:19:15 +0200

    test menu sticky index

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,17 @@
 $(document).foundation()
 
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+     //console.log(scroll);
+    if (scroll >= 100) {
+        //console.log('a');
+        $(".top-bar").removeClass("menu-fixe").addClass("menu-scroll");
+    } else {
+        //console.log('a');
+        $(".top-bar").removeClass("menu-scroll").addClass("menu-fixe");
+    }
+});
+
 /*
 // more click
 $('.card-profile-stats-more-link').click(function(e){
@@ -11,5 +23,18 @@ $('.card-profile-stats-more-link').click(function(e){
   }
   $(this).next('.card-profile-stats-more-content').slideToggle();
 });
+
+*/
+/*
+var prev = 0;
+var $window = $(window);
+var nav = $('.scrollhide-nav');
+
+$window.on('scroll', function(){
+  var scrollTop = $window.scrollTop();
+  nav.toggleClass('hidden', scrollTop > prev);
+  prev = scrollTop;
+});
+
 
 */
