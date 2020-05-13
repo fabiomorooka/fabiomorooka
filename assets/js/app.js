bf14c3e31db079b6e151581d524c786f66ffb6d3
fabiomorooka <fabio.morooka@gmail.com>
2020-05-14 00:00:55 +0200

    insert button

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,90 @@ $(window).scroll(function() {
     }
 });
 
+function ChangeFilter() {
+    var programmingSkills = document.querySelectorAll('[id=programming]');
+    var mechatronicsSkills = document.querySelectorAll('[id=mechatronics]');
+    var embeddedSystemsSkills = document.querySelectorAll('[id=embedded-systems]');
+    var softSkillSkills = document.querySelectorAll('[id=soft-skill]');
+
+    if(document.getElementById("programming-checkbox").checked){
+
+        for(var i=0; i<programmingSkills.length; i++) {
+            programmingSkills[i].style.display = 'block'
+        }
+        for(var i=0; i<mechatronicsSkills.length; i++){
+            mechatronicsSkills[i].style.display = 'none'
+        }
+        for(var i=0; i<embeddedSystemsSkills.length; i++){
+            embeddedSystemsSkills[i].style.display = 'none'
+        }
+        for(var i=0; i<softSkillSkills.length; i++){
+            softSkillSkills[i].style.display = 'none'
+        }
+    }
+    if(document.getElementById("mechatronics-checkbox").checked){
+        for(var i=0; i<programmingSkills.length; i++) {
+            programmingSkills[i].style.display = 'none'
+        }
+        for(var i=0; i<mechatronicsSkills.length; i++){
+            mechatronicsSkills[i].style.display = 'block'
+        }
+        for(var i=0; i<embeddedSystemsSkills.length; i++){
+            embeddedSystemsSkills[i].style.display = 'none'
+        }
+        for(var i=0; i<softSkillSkills.length; i++){
+            softSkillSkills[i].style.display = 'none'
+        }
+    }
+
+    if(document.getElementById("embedded-systems-checkbox").checked){
+        for(var i=0; i<programmingSkills.length; i++) {
+            programmingSkills[i].style.display = 'none'
+        }
+        for(var i=0; i<mechatronicsSkills.length; i++){
+            mechatronicsSkills[i].style.display = 'none'
+        }
+        for(var i=0; i<embeddedSystemsSkills.length; i++){
+            embeddedSystemsSkills[i].style.display = 'block'
+        }
+        for(var i=0; i<softSkillSkills.length; i++){
+            softSkillSkills[i].style.display = 'none'
+        }
+    }
+
+    if(document.getElementById("soft-skill-checkbox").checked){
+        for(var i=0; i<programmingSkills.length; i++) {
+            programmingSkills[i].style.display = 'none'
+        }
+        for(var i=0; i<mechatronicsSkills.length; i++){
+            mechatronicsSkills[i].style.display = 'none'
+        }
+        for(var i=0; i<embeddedSystemsSkills.length; i++){
+            embeddedSystemsSkills[i].style.display = 'none'
+        }
+        for(var i=0; i<softSkillSkills.length; i++){
+            softSkillSkills[i].style.display = 'block'
+        }
+    }
+
+    if(document.getElementById("all-checkbox").checked){
+        for(var i=0; i<programmingSkills.length; i++) {
+            programmingSkills[i].style.display = 'block'
+        }
+        for(var i=0; i<mechatronicsSkills.length; i++){
+            mechatronicsSkills[i].style.display = 'block'
+        }
+        for(var i=0; i<embeddedSystemsSkills.length; i++){
+            embeddedSystemsSkills[i].style.display = 'block'
+        }
+        for(var i=0; i<softSkillSkills.length; i++){
+            softSkillSkills[i].style.display = 'block'
+        }
+    }
+
+
+}
+
 /*
 // more click
 $('.card-profile-stats-more-link').click(function(e){
